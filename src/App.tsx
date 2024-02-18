6bc230747fd1d6d1caf103de897a4e71561f3f8c
RajeevKhati <rajeev.khati@nagarro.com>
2024-02-18 17:03:31 +0530

    feat(header): create header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/header";
+import Home from "./pages/home";
+import Compiler from "./pages/compiler";
+import NotFound from "./pages/not-found";
+import { ThemeProvider } from "./components/theme-provider";
+
 function App() {
-  return <>Hello</>;
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/compiler" element={<Compiler />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </ThemeProvider>
+  );
 }
 
 export default App;
